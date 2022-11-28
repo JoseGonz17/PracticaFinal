f3aa2a05279c5b78425caa94202b3f185ef101dd
JoseGon17 <josegonzalezgonzalez.17.11.2002@gmail.com>
2022-11-28 19:57:02 +0100

    28/11/2022

--- PracticaFinalJoseGonzalez/Assets/Scripts/GameDuck/Duck/DuckMovement.cs
+++ PracticaFinalJoseGonzalez/Assets/Scripts/GameDuck/Duck/DuckMovement.cs
@@ -7,4 +7,17 @@ public class DuckMovement : MonoBehaviour
     public Transform[] startPosition;
     public Transform[] standByPosition;
     public Transform[] endPosition;
+
+    public GameObject duckFirstMove;
+    private float duckStandByPosition;
+    private float duckStartPosition;
+
+    private void Start()
+    {
+    }
+
+    public void RespawnRandomPosition()
+    {
+        duckFirstMove.transform.position = new Vector3(0, Random.Range(duckStartPosition, duckStandByPosition), 0);
+    }
 }
