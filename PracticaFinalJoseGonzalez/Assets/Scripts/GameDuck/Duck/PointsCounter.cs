ef34f73482daede4b6e9076b297a0f77b4aff3c4
JoseGon17 <josegonzalezgonzalez.17.11.2002@gmail.com>
2022-12-16 18:38:18 +0100

    16/12/2002 2

--- PracticaFinalJoseGonzalez/Assets/Scripts/GameDuck/Duck/PointsCounter.cs
+++ PracticaFinalJoseGonzalez/Assets/Scripts/GameDuck/Duck/PointsCounter.cs
@@ -5,17 +5,36 @@ using UnityEngine.UI;
 
 public class PointsCounter : MonoBehaviour
 {
+    public static PointsCounter instance;
     public Text pointsText;
-    public float point;
+    public float point = 1;
+    public float pointsCurrents;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        pointsCurrents = point;
+    }
 
     private void Update()
     {
-        //pointsText.text = point.ToString();
+        if (gameObject.CompareTag("Duck") && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            pointsText.text = pointsCurrents.ToString();
+            pointsCurrents += point;
+        }
     }
-    public void GetPoint()
+
+    private void OnMouseDown()
     {
-        point += 1;
+        if (gameObject.CompareTag("Duck") && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            pointsText.text = pointsCurrents.ToString();
+            pointsCurrents += point;
+        }
     }
-
-
 }
